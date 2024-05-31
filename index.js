2370100ad0edfd1ce27cacf803c8c3ab16ac9f99
bieben <yitezheng13@gmail.com>
2024-05-31 21:34:53 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,53 @@
+import { OpenAI } from "openai";
+import express from 'express';
+import { HttpsProxyAgent } from 'https-proxy-agent';
+import bodyParser from "body-parser";
+import cors from "cors";
+const apiKey = process.env.API_KEY;
+const openai = new OpenAI({
+    apiKey: apiKey
+});
+
+async function main() {
+    const completion = await openai.chat.completions.create({
+        messages: [{ role: "user", content: "You are a helpful assistant." }],
+        model: "gpt-3.5-turbo",
+    },
+        {
+            proxy: false,
+            httpAgent: new HttpsProxyAgent('http://127.0.0.1:7890'),
+            httpsAgent: new HttpsProxyAgent('http://127.0.0.1:7890')
+        }
+    );
+
+    console.log(completion.choices[0]);
+}
+
+
+// main();
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post('/', async (req, res) => {
+    const { message } = req.body;
+    const completion = await openai.chat.completions.create({
+        messages: [{ role: "user", content: `${message}` }],
+        model: "gpt-3.5-turbo",
+    },
+        {
+            proxy: false,
+            httpAgent: new HttpsProxyAgent('http://127.0.0.1:7890'),
+            httpsAgent: new HttpsProxyAgent('http://127.0.0.1:7890')
+        }
+    );
+
+    res.json({
+        message: completion.choices[0].message.content
+    });
+});
+
+app.listen(3080, () => {
+    console.log("We've now got a server!");
+    console.log('Your routes will be running on http://localhost:3080');
+})
