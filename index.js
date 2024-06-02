be814085fdd8bb0635775ac763335fe37c361ca3
bieben <yitezheng13@gmail.com>
2024-06-03 00:16:11 +0800

    remove proxyurl

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 import { OpenAI } from "openai";
 import express from 'express';
-import { HttpsProxyAgent } from 'https-proxy-agent';
 import bodyParser from "body-parser";
 import cors from "cors";
 
 const apiKey = process.env.API_KEY;
-const proxyUrl = process.env.PROXY_URL || 'http://127.0.0.1:7890';
 
 const openai = new OpenAI({
     apiKey: apiKey
@@ -17,11 +15,6 @@ app.use(cors());
 
 app.post('/', async (req, res) => {
     const { message } = req.body;
-    const options = proxyUrl ? {
-        proxy: false,
-        httpAgent: new HttpsProxyAgent(proxyUrl),
-        httpsAgent: new HttpsProxyAgent(proxyUrl)
-    } : {};
     const completion = await openai.chat.completions.create({
         messages: [{ role: "user", content: `${message}` }],
         model: "gpt-3.5-turbo",
@@ -31,8 +24,7 @@ app.post('/', async (req, res) => {
         message: completion.choices[0].message.content
     });
 });
-console.log(process.env.PORT);
-console.log(process.env.API_KEY);
+
 const PORT = process.env.PORT || 3080;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
