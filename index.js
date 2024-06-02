774d3971b45c911081dfe266f5e605924dc6bbec
bieben <yitezheng13@gmail.com>
2024-06-02 23:51:12 +0800

    update env

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ import express from 'express';
 import { HttpsProxyAgent } from 'https-proxy-agent';
 import bodyParser from "body-parser";
 import cors from "cors";
+
 const apiKey = process.env.API_KEY;
+const proxyUrl = process.env.PROXY_URL || 'http://127.0.0.1:7890';
+
 const openai = new OpenAI({
     apiKey: apiKey
 });
@@ -14,23 +17,22 @@ app.use(cors());
 
 app.post('/', async (req, res) => {
     const { message } = req.body;
+    const options = proxyUrl ? {
+        proxy: false,
+        httpAgent: new HttpsProxyAgent(proxyUrl),
+        httpsAgent: new HttpsProxyAgent(proxyUrl)
+    } : {};
     const completion = await openai.chat.completions.create({
         messages: [{ role: "user", content: `${message}` }],
         model: "gpt-3.5-turbo",
-    },
-        {
-            proxy: false,
-            httpAgent: new HttpsProxyAgent('http://127.0.0.1:7890'),
-            httpsAgent: new HttpsProxyAgent('http://127.0.0.1:7890')
-        }
-    );
+    }, options);
 
     res.json({
         message: completion.choices[0].message.content
     });
 });
 
-app.listen(3080, () => {
-    console.log("We've now got a server!");
-    console.log('Your routes will be running on http://localhost:3080');
-})
+const PORT = process.env.PORT || 3080;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
