3d0d01e709ec8f4f3b61166c48b5d725f04c66c6
bieben <yitezheng13@gmail.com>
2024-06-03 00:04:01 +0800

    update procfile

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -32,7 +32,7 @@ function Chat() {
     setChatLog(chatLogNew);
     const messages = chatLogNew.map((message) => message.message).join("\n");
 
-    const response = await fetch("http://localhost:3080", {
+    const response = await fetch("https://my-gpt-server-8a49a44d5273.herokuapp.com", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
