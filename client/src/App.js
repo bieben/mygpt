c734072283d258b443012e9ec79b60f772d4cdb3
bieben <yitezheng13@gmail.com>
2024-06-02 23:55:36 +0800

    remove google provider

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,6 @@ import { useEffect } from 'react';
 import { auth } from './firebase';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import { useStateValue } from './StateProvider';
-import { GoogleOAuthProvider } from '@react-oauth/google';
 
 import Login from './components/Login/Login'
 import Chat from './components/Chat/Chat'
@@ -21,15 +20,13 @@ function App() {
       });
     });
   }, []);
-  
+
   return (
     <Router>
       <div className="app">
         <Routes>
           <Route path='/' element={
-            <GoogleOAuthProvider clientId="304980670815-7p09d3tvae5kjodi3kigaue0fvjh4keb.apps.googleusercontent.com">
-              <Login />
-            </GoogleOAuthProvider>
+            <Login />
           } />
           <Route path='/chat' element={<Chat />} />
         </Routes>
